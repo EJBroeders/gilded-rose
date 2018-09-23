d3863eb52504aa522b7af30973d4c4d8e9514428
Emile Broeders <ejbroeders@gmail.com>
2018-09-23 14:22:47 +0200

    Implemented regression tests

--- scala/src/test/scala/com/gildedrose/GildedRoseTest.scala
+++ scala/src/test/scala/com/gildedrose/GildedRoseTest.scala
@@ -2,11 +2,149 @@ package com.gildedrose
 
 import org.scalatest._
 
-class GildedRoseTest  extends FlatSpec with Matchers {
-      it should "foo" in {
-        var items = Array[Item](new Item("foo", 0, 0))
-        val app = new GildedRose(items)
-        app.updateQuality()
-        (app.items(0).name) should equal ("fixme")
-      }
+class GildedRoseTest extends FlatSpec with Matchers {
+
+  def mixedItems = new {
+      val items = Array[Item](
+        new Item("+5 Dexterity Vest", 10, 20),
+        new Item("Cheap +5 Dexterity Vest", 10, 1),
+        new Item("Aged Brie", 2, 0),
+        new Item("Aged Brie", 2, 49),
+        new Item("Elixir of the Mongoose", 5, 7),
+        new Item("Elixir of the Mongoose over due", 0, 7),
+        new Item("Sulfuras, Hand of Ragnaros", 0, 80),
+        new Item("Sulfuras, Hand of Ragnaros", -1, 80),
+        new Item("Backstage passes to a TAFKAL80ETC concert", 15, 20),
+        new Item("Backstage passes to a TAFKAL80ETC concert", 10, 49),
+        new Item("Backstage passes to a TAFKAL80ETC concert", 5, 49),
+        new Item("Conjured Mana Cake", 3, 6)
+    )
+    val test_items = items.map(_.copy())
+    val app = new GildedRose(test_items)
+  }
+
+  it should "construct the items array with the proper order" in {
+    val f = mixedItems
+    (f.app.items.map(_.name)) should equal (f.items.map(_.name))
+  }
+
+  it should "lower after each day the sellIn" in {
+    val f = mixedItems
+    f.app.updateQuality()
+    (f.app.items.map(_.sellIn)) should equal (f.items.map(_.sellIn - 1))
+  }
+
+  it should "never degrade the quality of items to negative" in {
+    val f = mixedItems
+    f.app.updateQuality()
+    f.app.updateQuality()
+    f.app.updateQuality()
+    f.app.updateQuality()
+    (f.app.items.filter(_.quality < 0).length) should equal (0)
+  }
+
+  it should "keep the quality of items always below 50 or have the quality equal 80 for legendary items" in {
+    val f = mixedItems
+    f.app.updateQuality()
+    f.app.updateQuality()
+    f.app.updateQuality()
+    f.app.updateQuality()
+    (f.app.items.filter(i => i.quality > 50 && i.quality != 80).length) should equal (0)
+  }
+
+  def regularItems = new {
+    val items = Array[Item](
+      new Item("+5 Dexterity Vest", 10, 20),
+      new Item("Cheap +5 Dexterity Vest", 10, 1),
+      new Item("Elixir of the Mongoose", 5, 7),
+      new Item("Elixir of the Mongoose over due", 0, 7)
+    )
+    val test_items = items.map(_.copy())
+    val app = new GildedRose(test_items)
+  }
+
+  it should "degrade the quality of regular items by one if sellIn > 0" in {
+    val f = regularItems
+    f.app.updateQuality()
+    (f.app.items.filter(_.sellIn >= 0).map(_.quality)) should equal (f.items.filter(_.sellIn > 0).map(_.quality - 1))
+  }
+
+  it should "degrade the quality of regular items by two if sellIn <= 0" in {
+    val f = regularItems
+    f.app.updateQuality()
+    (f.app.items.filter(_.sellIn < 0).map(_.quality)) should equal (f.items.filter(_.sellIn <= 0).map(_.quality - 2))
+  }
+
+  def brieItems = new {
+    val items = Array[Item](
+      new Item("Aged Brie", 2, 0),
+      new Item("Aged Brie", 2, 49)
+    )
+    val test_items = items.map(_.copy())
+    val app = new GildedRose(test_items)
+  }
+
+  it should "upgrade the quality of 'Aged Brie' by one if quality < 50" in {
+    val f = brieItems
+    f.app.updateQuality()
+    (f.app.items.filter(_.quality < 50).map(_.quality)) should equal (f.items.filter(_.quality < 49).map(_.quality + 1))
+  }
+
+  def sulfurasItems = new {
+    val items = Array[Item](
+      new Item("Sulfuras, Hand of Ragnaros", 0, 80),
+      new Item("Sulfuras, Hand of Ragnaros", -1, 80)
+    )
+    val test_items = items.map(_.copy())
+    val app = new GildedRose(test_items)
+  }
+
+  it should "keep the quality of 'Sulfuras, Hand of Ragnaros' invariant" in {
+    val f = sulfurasItems
+    f.app.updateQuality()
+    f.app.updateQuality()
+    f.app.updateQuality()
+    f.app.updateQuality()
+    (f.app.items.map(_.quality)) should equal (f.items.map(_.quality))
+  }
+
+  def backstageItems = new {
+    val items = Array[Item](
+      new Item("Backstage passes to a TAFKAL80ETC concert", 15, 20),
+      new Item("Backstage passes to a TAFKAL80ETC concert", 10, 49),
+      new Item("Backstage passes to a TAFKAL80ETC concert", 10, 40),
+      new Item("Backstage passes to a TAFKAL80ETC concert", 5, 49),
+      new Item("Backstage passes to a TAFKAL80ETC concert", 5, 40),
+      new Item("Backstage passes to a TAFKAL80ETC concert", 1, 49),
+      new Item("Backstage passes to a TAFKAL80ETC concert", 0, 49)
+    )
+    val test_items = items.map(_.copy())
+    val app = new GildedRose(test_items)
+  }
+
+  it should "upgrade the quality of 'Backstage passes to a TAFKAL80ETC concert' by one if sellIn > 10" in {
+    val f = backstageItems
+    f.app.updateQuality()
+    (f.app.items.filter(_.sellIn >= 10).map(_.quality)) should equal (f.items.filter(_.sellIn > 10).map(_.quality + 1))
+  }
+
+  it should "upgrade the quality of 'Backstage passes to a TAFKAL80ETC concert' by two if 5 < sellIn <= 10" in {
+    val f = backstageItems
+    f.app.updateQuality()
+    (f.app.items.filter(i => (i.sellIn < 10) & (i.sellIn >= 5)).map(_.quality)) should equal (
+      f.items.filter(i => (i.sellIn <= 10) & (i.sellIn > 5)).map(i => (i.quality + 2).min(50)))
+  }
+
+  it should "upgrade the quality of 'Backstage passes to a TAFKAL80ETC concert' by three if 0 < sellIn <= 5" in {
+    val f = backstageItems
+    f.app.updateQuality()
+    (f.app.items.filter(i => (i.sellIn < 5) & (i.sellIn >= 0)).map(_.quality)) should equal (
+      f.items.filter(i => (i.sellIn <= 5) & (i.sellIn > 0)).map(i => (i.quality + 3).min(50)))
+  }
+
+  it should "degrade the quality of 'Backstage passes to a TAFKAL80ETC concert' to 0 if sellIn <= 0" in {
+    val f = backstageItems
+    f.app.updateQuality()
+    (f.app.items.filter(_.sellIn < 0).map(_.quality)) should equal (f.items.filter(_.sellIn <= 0).map(i => 0))
+  }
 }
