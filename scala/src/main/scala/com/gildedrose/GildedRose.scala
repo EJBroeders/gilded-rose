e89d5e23084eacad613a50b8c90f28f9bd04e2f6
Emile Broeders <ejbroeders@gmail.com>
2018-09-23 14:54:08 +0200

    Refactored earlier code

--- scala/src/main/scala/com/gildedrose/GildedRose.scala
+++ scala/src/main/scala/com/gildedrose/GildedRose.scala
@@ -1,58 +1,38 @@
 package com.gildedrose
 
-class GildedRose(val items: Array[Item]) {
+class GildedRose(var items: Array[Item]) {
 
 
   def updateQuality() {
-    for (i <- 0 until items.length) {
-      if (!items(i).name.equals("Aged Brie")
-        && !items(i).name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-        if (items(i).quality > 0) {
-          if (!items(i).name.equals("Sulfuras, Hand of Ragnaros")) {
-            items(i).quality = items(i).quality - 1
-          }
+    items.map(item => {
+
+      item match {
+        case i if i.name.equals("Aged Brie") => {
+          i.sellIn = i.sellIn - 1
+          i.quality = (i.quality + 1).min(50)
         }
-      } else {
-        if (items(i).quality < 50) {
-          items(i).quality = items(i).quality + 1
 
-          if (items(i).name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-            if (items(i).sellIn < 11) {
-              if (items(i).quality < 50) {
-                items(i).quality = items(i).quality + 1
-              }
-            }
-
-            if (items(i).sellIn < 6) {
-              if (items(i).quality < 50) {
-                items(i).quality = items(i).quality + 1
-              }
-            }
-          }
+        case i if i.name.equals("Backstage passes to a TAFKAL80ETC concert") => {
+          i.sellIn = i.sellIn - 1
+          i.quality = (i.sellIn match {
+            case s if s >= 10 => i.quality + 1
+            case s if s < 10 & s >= 5 => i.quality + 2
+            case s if s < 5 & s >= 0 => i.quality + 3
+            case _ => 0
+          }).min(50)
         }
-      }
 
-      if (!items(i).name.equals("Sulfuras, Hand of Ragnaros")) {
-        items(i).sellIn = items(i).sellIn - 1
-      }
+        case i if i.name.equals("Sulfuras, Hand of Ragnaros") => {}
 
-      if (items(i).sellIn < 0) {
-        if (!items(i).name.equals("Aged Brie")) {
-          if (!items(i).name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-            if (items(i).quality > 0) {
-              if (!items(i).name.equals("Sulfuras, Hand of Ragnaros")) {
-                items(i).quality = items(i).quality - 1
-              }
-            }
-          } else {
-            items(i).quality = items(i).quality - items(i).quality
-          }
-        } else {
-          if (items(i).quality < 50) {
-            items(i).quality = items(i).quality + 1
-          }
+        case i => {
+          i.sellIn = i.sellIn - 1
+          i.quality = (i.sellIn match {
+            case s if s >= 0 => i.quality - 1
+            case _ => i.quality - 2
+          }).max(0)
         }
       }
-    }
+
+    })
   }
 }
