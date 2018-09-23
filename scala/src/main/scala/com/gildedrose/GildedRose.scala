ad0696cb92ba960aad6b7affc07ceb448c73f858
Emile Broeders <ejbroeders@gmail.com>
2018-09-23 15:13:25 +0200

    Made item within items within GildedRose immutable (this should please the imp)

--- scala/src/main/scala/com/gildedrose/GildedRose.scala
+++ scala/src/main/scala/com/gildedrose/GildedRose.scala
@@ -4,40 +4,50 @@ class GildedRose(var items: Array[Item]) {
 
 
   def updateQuality() {
-    items.map(item => {
+    items = items.map(item => {
 
       item match {
         case i if i.name.equals("Aged Brie") => {
-          i.sellIn = i.sellIn - 1
-          i.quality = (i.quality + 1).min(50)
+          item.copy(
+            sellIn = item.sellIn - 1,
+            quality = (item.quality + 1).min(50)
+          )
         }
 
         case i if i.name.equals("Backstage passes to a TAFKAL80ETC concert") => {
-          i.sellIn = i.sellIn - 1
-          i.quality = (i.sellIn match {
-            case s if s >= 10 => i.quality + 1
-            case s if s < 10 & s >= 5 => i.quality + 2
-            case s if s < 5 & s >= 0 => i.quality + 3
-            case _ => 0
-          }).min(50)
+          item.copy(
+            sellIn = item.sellIn - 1,
+            quality = (item.sellIn match {
+              case s if s > 10 => item.quality + 1
+              case s if s <= 10 & s > 5 => item.quality + 2
+              case s if s <= 5 & s > 0 => item.quality + 3
+              case _ => 0
+            }).min(50)
+          )
         }
 
-        case i if i.name.equals("Sulfuras, Hand of Ragnaros") => {}
+        case i if i.name.equals("Sulfuras, Hand of Ragnaros") => {
+          item.copy()
+        }
 
         case i if i.name.startsWith("Conjured") => {
-          i.sellIn = i.sellIn - 1
-          i.quality = (i.sellIn match {
-            case s if s >= 0 => i.quality - 2
-            case _ => i.quality - 4
-          }).max(0)
+          item.copy(
+            sellIn = item.sellIn - 1,
+            quality = (item.sellIn match {
+              case s if s > 0 => item.quality - 2
+              case _ => item.quality - 4
+            }).max(0)
+          )
         }
 
-        case i => {
-          i.sellIn = i.sellIn - 1
-          i.quality = (i.sellIn match {
-            case s if s >= 0 => i.quality - 1
-            case _ => i.quality - 2
-          }).max(0)
+        case _ => {
+          item.copy(
+            sellIn = item.sellIn - 1,
+            quality = (item.sellIn match {
+              case s if s > 0 => item.quality - 1
+              case _ => item.quality - 2
+            }).max(0)
+          )
         }
       }
 
